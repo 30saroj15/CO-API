ff78aff1156485c177374d28e01bcba9935480a4
adhik <adhik@DESKTOP-M1O4T13>
2021-02-01 22:24:26 -0500

    wh 567 commit

--- src/main/java/com/sa/entity/COPdfs.java
+++ src/main/java/com/sa/entity/COPdfs.java
@@ -1,19 +1,15 @@
 package com.sa.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import java.sql.Blob;
 
-import lombok.Data;
-
-@Entity
-@Data
-@Table(name = "CO_PDFS")
 public class COPdfs {
-
+	
 	int co_pdf_id;
 	String plan_status;
-	String case_number;
-	String pdf_docunment;
-	String plan_name;
+	int case_number;
+	Blob pdf_document;
+	String plane_name;
+	        
+
 
 }
